e31675cabf9e791f69c7c8abe584724718367d4e
smand54 <sayari.mandal@aexp.com>
2021-04-19 02:01:29 +0530

    16
    Adding Auth to application

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,4 +1,10 @@
 import { config, createSchema } from "@keystone-next/keystone/schema";
+import { createAuth } from "@keystone-next/auth";
+import {
+	withItemData,
+	statelessSessions,
+} from "@keystone-next/keystone/session";
+
 import "dotenv/config";
 import { User } from "./schemas/User";
 
@@ -10,26 +16,43 @@ const sessionConfig = {
 	secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-	// @ts-ignore
-	server: {
-		cors: {
-			origin: [process.env.FRONTEND_URL],
-			credentials: true,
-		},
+const { withAuth } = createAuth({
+	listKey: "User",
+	identityField: "email",
+	secretField: "password",
+	initFirstItem: {
+		fields: ["name", "email", "password"],
+		// TODO: Add in inital roles here
 	},
-	db: {
-		adapter: "mongoose",
-		url: databaseURL,
-		// TODO: Add data seeding here
-	},
-	lists: createSchema({
-		// Schema items go in here
-		User,
-	}),
-	ui: {
-		// TODO: change this for roles
-		isAccessAllowed: () => true,
-	},
-	// TODO: Add session values here
 });
+
+export default withAuth(
+	config({
+		// @ts-ignore
+		server: {
+			cors: {
+				origin: [process.env.FRONTEND_URL],
+				credentials: true,
+			},
+		},
+		db: {
+			adapter: "mongoose",
+			url: databaseURL,
+			// TODO: Add data seeding here
+		},
+		lists: createSchema({
+			// Schema items go in here
+			User,
+		}),
+		ui: {
+			// Show the UI only for poeple who pass this test
+			isAccessAllowed: ({ session }) =>
+				// console.log(session);
+				!!session?.data,
+		},
+		session: withItemData(statelessSessions(sessionConfig), {
+			// GraphQL Query
+			User: "id name email",
+		}),
+	})
+);
